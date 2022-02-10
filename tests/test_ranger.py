b2bc7620b26d33054f88967b8419eca76453a40b
Jeremy Deram <jderam@gmail.com>
2022-02-10 03:33:41 -0600

    fix mypy complaints

--- tests/test_ranger.py
+++ tests/test_ranger.py
@@ -3,5 +3,7 @@ from hyperborea3.chargen import get_spells
 
 def test_level7_spells():
     spells = get_spells(10, 7, 1)
-    assert list(spells["drd"].keys()) == ["spells_per_day", "spells_known"]
-    assert list(spells["mag"].keys()) == ["spells_per_day", "spells_known"]
+    assert spells is not None
+    if spells is not None:
+        assert list(spells["drd"].keys()) == ["spells_per_day", "spells_known"]
+        assert list(spells["mag"].keys()) == ["spells_per_day", "spells_known"]
