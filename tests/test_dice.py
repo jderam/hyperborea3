9624f811040ee86522b15751adf25c74caeaedfa
Jeremy Deram <jderam@gmail.com>
2022-02-11 04:33:01 -0600

    added test for dice method V

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,4 +1,7 @@
-from hyperborea3.chargen import roll_dice
+import pytest
+import random
+
+from hyperborea3.chargen import roll_dice, roll_stats
 
 
 def test_3d6():
@@ -6,3 +9,10 @@ def test_3d6():
     assert min(results) == 3
     assert max(results) == 18
     assert 10.4 <= sum(results) / len(results) <= 10.6
+
+
+@pytest.mark.repeat(1000)
+def test_dice_method5():
+    attr = roll_stats(method=5, class_id=random.randint(1, 4))
+    for stat in attr:
+        assert 8 <= attr[stat]["score"] <= 18
