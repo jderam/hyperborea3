bd2ed91e10e0d51c1cc16df96e2ae07537d0807d
Jeremy Deram <jderam@gmail.com>
2022-02-21 11:35:44 -0600

    adding all the files

---
+++ scripts/spell_desc/lvl1/create_spell_files.py
@@ -0,0 +1,12 @@
+
+
+with open("spell_names.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    data = line.split()
+    spell_id = data[0]
+    file_name = ("_").join(data[1:]) + ".yml"
+    print(file_name)
+    with open(file_name, "w") as sf:
+        sf.write(f"id: {spell_id}\n")
