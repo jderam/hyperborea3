664f640bac622d0301ad7824b2530d04e45b101f
Jeremy Deram <jderam@gmail.com>
2022-02-21 11:44:53 -0600

    update scripts

--- scripts/spell_desc/create_spell_files.py
+++ scripts/spell_desc/create_spell_files.py
@@ -1,12 +1,12 @@
+subdir = "lvl1"
 
-
-with open("spell_names.txt", "r") as f:
+with open(f"{subdir}/spell_names.txt", "r") as f:
     lines = f.readlines()
 
 for line in lines:
     data = line.split()
     spell_id = data[0]
-    file_name = ("_").join(data[1:]) + ".yml"
+    file_name = f"{subdir}/{('_').join(data[1:])}.yml"
     print(file_name)
     with open(file_name, "w") as sf:
         sf.write(f"id: {spell_id}\n")
