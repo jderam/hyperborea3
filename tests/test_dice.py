c01c55de93057aa79c46c64054f3c31cb4a044e9
Jeremy Deram <jderam@gmail.com>
2022-12-31 18:58:12 -0600

    add tests for ndn drop highest/lowest

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,7 +1,16 @@
 import pytest
 import random
 
-from hyperborea3.chargen import roll_dice, roll_stats
+from hyperborea3.chargen import (
+    roll_dice,
+    roll_ndn_drop_lowest,
+    roll_ndn_drop_highest,
+    roll_stats,
+)
+
+AVG_4D6_DROP_LOWEST = 12.2446
+AVG_4D6_DROP_HIGHEST = 8.7554
+TOLERANCE = 0.1
 
 
 def test_3d6():
@@ -20,6 +29,26 @@ def test_reroll():
 def test_reroll_exception():
     with pytest.raises(AssertionError):
         roll_dice(1, 6, reroll=[1, 2, 3, 4, 5, 6])
+
+
+def test_ndn_drop_lowest():
+    sample = [roll_ndn_drop_lowest(4, 6, 1) for _ in range(10000)]
+    average = sum(sample) / len(sample)
+    assert (
+        (AVG_4D6_DROP_LOWEST - TOLERANCE)
+        <= average
+        <= (AVG_4D6_DROP_LOWEST + TOLERANCE)
+    )
+
+
+def test_ndn_drop_highest():
+    sample = [roll_ndn_drop_highest(4, 6, 1) for _ in range(10000)]
+    average = sum(sample) / len(sample)
+    assert (
+        (AVG_4D6_DROP_HIGHEST - TOLERANCE)
+        <= average
+        <= (AVG_4D6_DROP_HIGHEST + TOLERANCE)
+    )
 
 
 @pytest.mark.repeat(1000)
