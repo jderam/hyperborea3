e83a0d4a7b58e6e79bd4f1f074807fd75a857f0e
Jeremy Deram <jderam@gmail.com>
2023-01-19 22:11:35 -0600

    add tests for PlayerCharacter.languages array

--- tests/test_pc.py
+++ tests/test_pc.py
@@ -10,6 +10,7 @@ from hyperborea3.valid_data import (
     VALID_FA,
     VALID_GENDERS,
     VALID_GP,
+    VALID_LANGUAGES,
     VALID_LEVELS,
     VALID_RACE_IDS,
     VALID_SAVES,
@@ -35,6 +36,13 @@ def test_pc():
     assert pc.weight.endswith(" lbs.")
     assert pc.secondary_skill in VALID_SECONDARY_SKILLS
     assert pc.alignment["short_name"] in VALID_ALIGMENTS_SHORT
+    if pc.attr["in"]["lang"] >= 0:
+        assert len(pc.languages) == pc.attr["in"]["lang"] + 1
+    else:
+        assert len(pc.languages) == 1
+    assert len(set(pc.languages)) == len(pc.languages)
+    for language in pc.languages:
+        assert language in VALID_LANGUAGES
     assert pc.method in VALID_DICE_METHODS
     assert pc.fa in VALID_FA
     assert pc.ca in VALID_CA
